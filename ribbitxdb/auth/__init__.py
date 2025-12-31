785d3b86989c7ed6815a87d45bdec29551543c4a
RibbitX <realmrhatt@gmail.com>
2025-12-31 18:06:20 +0100

    Delete ribbitxdb directory

--- ribbitxdb/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Authentication and Authorization Module
-"""
-
-from .user_manager import UserManager, User
-from .authenticator import Authenticator
-from .authorizer import Authorizer
-
-__all__ = ['UserManager', 'User', 'Authenticator', 'Authorizer']
